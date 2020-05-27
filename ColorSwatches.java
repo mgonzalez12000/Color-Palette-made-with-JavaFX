0e264768b0644fe211b047cd344f4b59723119c0
mgonzalez12000 <43226912+mgonzalez12000@users.noreply.github.com>
2020-05-26 21:23:16 -0700

    Add files via upload

---
+++ ColorSwatches.java
@@ -0,0 +1,249 @@
+package ColorSwatches;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class ColorSwatches extends Application {
+	
+	@Override
+	public void start(Stage primaryStage){
+		//red, blue, green, opacity
+		//Create rectangle object
+		Rectangle rectangle1 = new Rectangle();
+		rectangle1.setWidth(100);
+		rectangle1.setHeight(100);
+		rectangle1.setFill(new Color (1,0,0,0.1));
+		
+		//Create rectnagle2 object
+		Rectangle rectangle2 = new Rectangle();
+		rectangle2.setWidth(100);
+		rectangle2.setHeight(100);
+		rectangle2.setFill(new Color (1, 0, 0, 0.2));
+		
+		//Create rectnagle3 object
+		Rectangle rectangle3 = new Rectangle();
+		rectangle3.setWidth(100);
+		rectangle3.setHeight(100);
+		rectangle3.setFill(new Color (1, 0, 0, 0.3));
+		
+		//Create rectnagle4 object
+		Rectangle rectangle4 = new Rectangle();
+		rectangle4.setWidth(100);
+		rectangle4.setHeight(100);
+		rectangle4.setFill(new Color (1, 0, 0, 0.4));
+		
+		//Create ractangle5 object
+		Rectangle rectangle5 = new Rectangle();
+		rectangle5.setWidth(100);
+		rectangle5.setHeight(100);
+		rectangle5.setFill(new Color(1, 0, 0, 0.5));
+		
+		//Create ractangle6 object
+		Rectangle rectangle6 = new Rectangle();
+		rectangle6.setWidth(100);
+		rectangle6.setHeight(100);
+		rectangle6.setFill(new Color(1, 0, 0, 0.6));
+		
+		//Create rectangle7 object
+		Rectangle rectangle7 = new Rectangle();
+		rectangle7.setWidth(100);
+		rectangle7.setHeight(100);
+		rectangle7.setFill(new Color(0, 1, 1, 0.1));
+		
+		//Create rectangle8 object
+		Rectangle rectangle8 = new Rectangle();
+		rectangle8.setWidth(100);
+		rectangle8.setHeight(100);
+		rectangle8.setFill(new Color(0, 1, 1, 0.2));
+		
+		//Create rectangle9 object
+		Rectangle rectangle9 = new Rectangle();
+		rectangle9.setWidth(100);
+		rectangle9.setHeight(100);
+		rectangle9.setFill(new Color(0, 1, 1, 0.3));
+		
+		//Create rectangle10 object
+		Rectangle rectangle10 = new Rectangle();
+		rectangle10.setWidth(100);
+		rectangle10.setHeight(100);
+		rectangle10.setFill(new Color(0, 1, 1, 0.4));
+		
+		//Create rectangle11 object
+		Rectangle rectangle11 = new Rectangle();
+		rectangle11.setWidth(100);
+		rectangle11.setHeight(100);
+		rectangle11.setFill(new Color(0, 1, 1, 0.5));
+		
+		//Create rectangle12 object
+		Rectangle rectangle12 = new Rectangle();
+		rectangle12.setWidth(100);
+		rectangle12.setHeight(100);
+		rectangle12.setFill(new Color(0, 1, 1, 0.6));
+		
+		//Create ractangle13 object
+		Rectangle rectangle13 = new Rectangle();
+		rectangle13.setWidth(100);
+		rectangle13.setHeight(100);
+		rectangle13.setFill(new Color(1, 1, 0, 0.1));
+		
+		//Create ractangle14 object
+		Rectangle rectangle14 = new Rectangle();
+		rectangle14.setWidth(100);
+		rectangle14.setHeight(100);
+		rectangle14.setFill(new Color(1, 1, 0, 0.2));
+		
+		//Create ractangle15 object
+		Rectangle rectangle15 = new Rectangle();
+		rectangle15.setWidth(100);
+		rectangle15.setHeight(100);
+		rectangle15.setFill(new Color(1, 1, 0, 0.3));
+		
+		//Create ractangle16 object
+		Rectangle rectangle16 = new Rectangle();
+		rectangle16.setWidth(100);
+		rectangle16.setHeight(100);
+		rectangle16.setFill(new Color(1, 1, 0, 0.4));
+		
+		//Create ractangle17 object
+		Rectangle rectangle17 = new Rectangle();
+		rectangle17.setWidth(100);
+		rectangle17.setHeight(100);
+		rectangle17.setFill(new Color(1, 1, 0, 0.5));
+		
+		//Create ractangle18 object
+		Rectangle rectangle18 = new Rectangle();
+		rectangle18.setWidth(100);
+		rectangle18.setHeight(100);
+		rectangle18.setFill(new Color(1, 1, 0, 0.6));
+		
+		//Create ractangle19 object
+		Rectangle rectangle19 = new Rectangle();
+		rectangle19.setWidth(100);
+		rectangle19.setHeight(100);
+		rectangle19.setFill(new Color(0, 1, 0, 0.1));
+		
+		//Create ractangle20 object
+		Rectangle rectangle20 = new Rectangle();
+		rectangle20.setWidth(100);
+		rectangle20.setHeight(100);
+		rectangle20.setFill(new Color(0, 1, 0, 0.2));
+		
+		//Create ractangle21 object
+		Rectangle rectangle21 = new Rectangle();
+		rectangle21.setWidth(100);
+		rectangle21.setHeight(100);
+		rectangle21.setFill(new Color(0, 1, 0, 0.3));
+		
+		//Create ractangle22 object
+		Rectangle rectangle22 = new Rectangle();
+		rectangle22.setWidth(100);
+		rectangle22.setHeight(100);
+		rectangle22.setFill(new Color(0, 1, 0, 0.4));
+		
+		//Create ractangle23 object
+		Rectangle rectangle23 = new Rectangle();
+		rectangle23.setWidth(100);
+		rectangle23.setHeight(100);
+		rectangle23.setFill(new Color(0, 1, 0, 0.5));
+		
+		//Create ractangle24 object
+		Rectangle rectangle24 = new Rectangle();
+		rectangle24.setWidth(100);
+		rectangle24.setHeight(100);
+		rectangle24.setFill(new Color(0, 1, 0, 0.6));
+		
+		//Create ractangle25 object
+		Rectangle rectangle25 = new Rectangle();
+		rectangle25.setWidth(100);
+		rectangle25.setHeight(100);
+		rectangle25.setFill(new Color(0, 0, 1, 0.1));
+		
+		//Create ractangle26 object
+		Rectangle rectangle26 = new Rectangle();
+		rectangle26.setWidth(100);
+		rectangle26.setHeight(100);
+		rectangle26.setFill(new Color(0, 0, 1, 0.2));
+		
+		//Create ractangle27 object
+		Rectangle rectangle27 = new Rectangle();
+		rectangle27.setWidth(100);
+		rectangle27.setHeight(100);
+		rectangle27.setFill(new Color(0, 0, 1, 0.3));
+		
+		//Create ractangle28 object
+		Rectangle rectangle28 = new Rectangle();
+		rectangle28.setWidth(100);
+		rectangle28.setHeight(100);
+		rectangle28.setFill(new Color(0, 0, 1, 0.4));
+		
+		//Create ractangle29 object
+		Rectangle rectangle29 = new Rectangle();
+		rectangle29.setWidth(100);
+		rectangle29.setHeight(100);
+		rectangle29.setFill(new Color(0, 0, 1, 0.5));
+		
+		//Create ractangle25 object
+		Rectangle rectangle30 = new Rectangle();
+		rectangle30.setWidth(100);
+		rectangle30.setHeight(100);
+		rectangle30.setFill(new Color(0, 0, 1, 0.6));
+		
+		//Create ractangle31 object
+		Rectangle rectangle31 = new Rectangle();
+		rectangle31.setWidth(100);
+		rectangle31.setHeight(100);
+		rectangle31.setFill(new Color(1, 0, 1, 0.1));
+		
+		//Create ractangle32 object
+		Rectangle rectangle32 = new Rectangle();
+		rectangle32.setWidth(100);
+		rectangle32.setHeight(100);
+		rectangle32.setFill(new Color(1, 0, 1, 0.2));
+		
+		//Create ractangle33 object
+		Rectangle rectangle33 = new Rectangle();
+		rectangle33.setWidth(100);
+		rectangle33.setHeight(100);
+		rectangle33.setFill(new Color(1, 0, 1, 0.3));
+		
+		//Create ractangle34 object
+		Rectangle rectangle34 = new Rectangle();
+		rectangle34.setWidth(100);
+		rectangle34.setHeight(100);
+		rectangle34.setFill(new Color(1, 0, 1, 0.4));
+		
+		//Create ractangle35 object
+		Rectangle rectangle35 = new Rectangle();
+		rectangle35.setWidth(100);
+		rectangle35.setHeight(100);
+		rectangle35.setFill(new Color(1, 0, 1, 0.5));
+		
+		//Create ractangle36 object
+		Rectangle rectangle36 = new Rectangle();
+		rectangle36.setWidth(100);
+		rectangle36.setHeight(100);
+		rectangle36.setFill(new Color(1, 0, 1, 0.6));
+		
+		//Implement FlowPane for colors
+		Pane root = new FlowPane();
+		root.getChildren().addAll(rectangle1, rectangle2, rectangle3, rectangle4, rectangle5, rectangle6, rectangle7, rectangle8, rectangle9, rectangle10, rectangle11, rectangle12, rectangle13, rectangle14, rectangle15, rectangle16, rectangle17, rectangle18, rectangle19, rectangle20, rectangle21, rectangle22, rectangle23, rectangle24, rectangle25, rectangle26, rectangle27, rectangle28, rectangle29, rectangle30, rectangle31, rectangle32, rectangle33, rectangle34, rectangle35, rectangle36);
+        
+        //Create title of page
+        primaryStage.setTitle("ColorSwatches");
+        //add the scene to the stage
+        primaryStage.setScene(new Scene (root, 600,600));
+        //show the contents of the stage 
+        primaryStage.show();
+    }
+    
+    //launch program
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ module-info.java
@@ -0,0 +1,9 @@
+module JavaFXHW01A {
+	//you must export your package module
+	exports ColorSwatches;
+	//the requires keyword is used to call external jars
+	requires javafx.base;
+	requires javafx.graphics;
+	requires javafx.controls;
+	requires javafx.swing;
+}
